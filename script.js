8ed1b8d8f86e8d7aa46e8c708e99aa3c4b975b60
Potassium <brainiacgamer444@gmail.com>
2023-08-21 17:11:54 +0545

    Complete making password check function

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+const passwordField = document.querySelectorAll(".passwordField");
 const passwordValue = document.getElementById("password");
 const confirmPasswordValue = document.getElementById("confirm_password");
+const errorMessage = document.querySelector(".error_message");
 
-confirmPasswordValue.addEventListener("change", ()=>{
-    if(passwordValue.textContent !== confirmPasswordValue.textContent){
-        console.log("no");
-    }else{
-        console.log("yes");
-    }
-})
+function checkPasswordMatch() {
+    passwordField.forEach((element) => {
+        element.addEventListener("input", () => {
+            if (passwordValue.value !== confirmPasswordValue.value || passwordField.value === "" || confirmPasswordValue.value === "") {
+                passwordValue.style.border = "2px solid rgb(255, 29, 29)";
+                confirmPasswordValue.style.border = "2px solid rgb(255, 29, 29)";
+                errorMessage.textContent = "Passwords do not match";
+            } else {
+                confirmPasswordValue.style.border = "2px solid rgb(54, 141, 255)";
+                passwordValue.style.border = "2px solid rgb(54, 141, 255)";
+                errorMessage.textContent = "";
+            }
+        })
+    })
+}
+checkPasswordMatch();
